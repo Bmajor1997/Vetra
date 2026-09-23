38706a11bb9ac0ecd32df3187f81bf124ef15d65
Benjamin Major <162641768+Bmajor1997@users.noreply.github.com>
2026-09-23 12:23:20 -0400

    Register Ask Votic route

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -2,4 +2,4 @@ import { Stack } from "expo-router";
 import { ThemeProvider } from "../src/theme/ThemeProvider";
 import { AccessibilityProvider } from "../src/accessibility/AccessibilityProvider";
 import { DocumentLibraryProvider } from "../src/documents/DocumentLibraryProvider";
-export default function RootLayout(){return <ThemeProvider><AccessibilityProvider><DocumentLibraryProvider><Stack screenOptions={{headerShown:false}}><Stack.Screen name="(tabs)"/><Stack.Screen name="reader"/></Stack></DocumentLibraryProvider></AccessibilityProvider></ThemeProvider>}
+export default function RootLayout(){return <ThemeProvider><AccessibilityProvider><DocumentLibraryProvider><Stack screenOptions={{headerShown:false}}><Stack.Screen name="(tabs)"/><Stack.Screen name="reader"/><Stack.Screen name="assistant"/></Stack></DocumentLibraryProvider></AccessibilityProvider></ThemeProvider>}
